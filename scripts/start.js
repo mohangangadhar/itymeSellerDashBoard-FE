19a05fe03563db6673879328822c9f4c63d3f8fa
Mohan Gangadhar Gudey <mohan.gangadhar@gmail.com>
2021-08-18 13:58:09 +0530

    removed chalk

--- scripts/start.js
+++ scripts/start.js
@@ -16,7 +16,7 @@ require('../config/env');
 
 
 const fs = require('fs');
-const chalk = require('chalk');
+// const chalk = require('chalk');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const clearConsole = require('react-dev-utils/clearConsole');
@@ -45,18 +45,12 @@ const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
 
 if (process.env.HOST) {
-  console.log(
-    chalk.cyan(
-      `Attempting to bind to HOST environment variable: ${chalk.yellow(
-        chalk.bold(process.env.HOST)
-      )}`
-    )
-  );
+  console.log( "Attempting to bind host variable");
   console.log(
     `If this was unintentional, check that you haven't mistakenly set it in your shell.`
   );
   console.log(
-    `Learn more here: ${chalk.yellow('http://bit.ly/CRA-advanced-config')}`
+    `Learn more here: http://bit.ly/CRA-advanced-config`
   );
   console.log();
 }
@@ -97,7 +91,7 @@ checkBrowsers(paths.appPath, isInteractive)
       if (isInteractive) {
         clearConsole();
       }
-      console.log(chalk.cyan('Starting the development server...\n'));
+      console.log('Starting the development server...\n');
       //openBrowser(urls.localUrlForBrowser);
     });
 
